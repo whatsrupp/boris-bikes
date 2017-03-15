813e06537951cd977f916a17bea1af9d4723bd83
Nick Rupp <nick@Nicks-MacBook-Pro.local>
2017-03-15 16:23:18 +0000

    Privatise guard clause conditions empty? and full?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,9 @@
 require_relative 'bike'
 
+
 class DockingStation
+  public
+
   attr_reader :bikes, :capacity
   def initialize
     @bikes = []
@@ -8,15 +11,27 @@ class DockingStation
   end
 
   def release_bike
-    fail "No Bikes" if @bikes.empty?
+    fail "No Bikes" if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail "Dock Full" if @bikes.length >= @capacity
+    fail "Dock Full" if full?
     @bikes << bike
 
   end
+
+  private
+
+  def empty?
+    @bikes.empty? ? true : false
+  end
+
+  def full?
+    @bikes.count >= @capacity ? true : false
+  end
+
+
 
 
 end
