a88a382768bd053608e66b78bd81b0d2a34ea5f1
Nick Rupp <nick@Nicks-MacBook-Pro.local>
2017-03-16 13:54:01 +0000

    Isolate DockingStation testing with doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,11 @@ require 'docking_station'
 
 describe DockingStation do
 
+  let (:bike) {double :bike}
+
   describe "#release_bike" do
 
-    it "releases a bike" do
+    it "Docking station has a method to release a bike" do
       is_expected.to respond_to :release_bike
     end
 
@@ -13,14 +15,13 @@ describe DockingStation do
     end
 
     it "releases a bike" do
-      bike = Bike.new
+      allow(bike).to receive(:working?).and_return(true)
       subject.bikes.push(bike)
       expect(subject.release_bike).to eq(bike)
     end
 
     it "raises bike broken if bike broken" do
-      bike = Bike.new
-      bike.break
+      allow(bike).to receive(:working?).and_return(false)
       subject.bikes.push(bike)
       expect {subject.release_bike}.to raise_error("Bike Broken")
     end
@@ -33,24 +34,21 @@ describe DockingStation do
     end
 
     it "checks bike has been added to existing bikes" do
-      bike = Bike.new
       expect(subject.dock(bike)).to eq (subject.bikes << bike)
     end
 
     it "Cannot dock if the station is full" do
-      subject.capacity.times {subject.dock(Bike.new)}
-      expect {subject.dock(Bike.new)}.to raise_error("Dock Full")
+      subject.capacity.times {subject.dock(bike)}
+      expect {subject.dock(bike)}.to raise_error("Dock Full")
     end
 
     it "Reports bike as not working when docked" do
-      bike = Bike.new
       subject.dock(bike)
       expect { bike.working?.to eq false}
     end
 
     it "Docks bike when bike is broken" do
-      bike = Bike.new
-      bike.break
+      allow(bike).to receive(:working?).and_return(false)
       subject.dock(bike)
       expect(subject.bikes.last).to eq bike
     end
